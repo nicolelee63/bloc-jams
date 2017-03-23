55dcf5d1bbda0c24f322414f939b352d2c46eb2b
nicolelee63 <nicole.lee633@gmail.com>
2017-03-24 01:46:25 +0800

    Checkpoint 12 Play Pause completed

--- scripts/album.js
+++ scripts/album.js
@@ -31,10 +31,25 @@ var albumPicasso = {
      ]
  };
 
+var albumDodgeball = {
+     title: 'Facial',
+     artist: 'Junkshot',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, bark', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Who dis', duration: '2:15'}
+     ]
+ };
+
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -66,6 +81,36 @@ var setCurrentAlbum = function(album) {
      }
  };
  
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = docuemnt.getElementsByClassName('album-view-song-item');
+
+// Album button templates
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+     
+     songListContainer.addEventListener('mouseover', function(event) {
+         // Only target individual song rows during event delegation
+         if (event.target.parentElement.className === 'album-view-song-item') {
+             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+     });
+     
+     for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+             // Selects first child element, which is song-item-number
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
+     
+     var albums = [albumPicasso,albumMarconi,albumDodgeball];
+     var index=1;
+     albumImage.addEventLister("click", function(event) {
+         setCurrentAlbum(albums[index]);
+         index++;
+         if (index == albums.length) {
+             index = 0;
+         }
+     });
  };
